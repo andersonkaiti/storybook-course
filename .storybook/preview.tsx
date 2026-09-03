ef09f1dfd996296c740facb36e63d5cd12081e5d
Anderson Kaiti <anderkaiti@gmail.com>
2026-09-03 20:25:08 -0300

    :lipstick: feat(storybook): add dark theme and load global styles

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,14 +1,17 @@
-import type { Preview } from '@storybook/react-vite'
+import { withThemeByClassName } from '@storybook/addon-themes'
+import type { Preview, ReactRenderer } from '@storybook/react-vite'
+import '../src/index.css'
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
+  decorators: [
+    withThemeByClassName<ReactRenderer>({
+      themes: {
+        light: '',
+        dark: 'dark',
       },
-    },
-  },
+      defaultTheme: 'dark',
+    }),
+  ],
 }
 
 export default preview
